f453a3ef8cf994917ef3bf39737b7696ef086c1d
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-09-17 17:08:18 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,63 @@
 def addition(num1, num2)
+  puts "Enter first number:"
+  num1 == gets.chomp()
+  puts "Enter second number:"
+  num2 == gets.chomp()
+  
+  if addition == "+"
+    puts (num1 + num2)
 
 end
 
 def subtraction(num1, num2)
+  puts "Enter first number:"
+  num1 == gets.chomp()
+  puts "Enter second number"
+  num2 == gets.chomp()
+  
+  if subtraction == "-"
+    puts (num1 - num2)
 
 end
 
 def division(num1, num2)
+  puts "Enter first number"
+  num1 == gets.chomp()
+  puts "Enter second number"
+  num2 == gets.chomp()
+  
+  if division == "/"
+    puts (num1 / num2)
 
 end
 
 def multiplication(num1, num2)
+  puts "Enter first number"
+  num1 == gets.chomp()
+  puts "Enter second number"
+  num2 == gets.chomp()
+  
+  if multiplication == "*"
+    puts (num1 * num2)
+    
 
 end
 
 def modulo(num1, num2)
+  puts "Enter first number"
+  num1 == gets.chomp()
+  puts "Enter second number"
+  num2 == gets.chomp()
+  
+  if modulo == %
+    puts (num %)
 
 end
 
 def square_root(num)
+  math.sqrt()
+  puts "Enter number"
+  sqrt = math.sqrt(num)
+  puts (num)
 
 end
