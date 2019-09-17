ac2542b56d25a00466d11bf1cf281de5ba006c45
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-09-17 16:36:08 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,45 @@
 def addition(num1, num2)
+  puts "Enter first number:"
+  num1 == gets.chomp()
+  puts "Enter second number:"
+  num2 == gets.chomp()
+  
+  if addition == "+"
+    puts (num1 + num2)
 
 end
 
 def subtraction(num1, num2)
+  puts "Enter first number:"
+  num1 == gets.chomp()
+  puts "Enter second number"
+  num2 == gets.chomp()
+  
+  if subtraction == "-"
+    puts (num1 - num2)
 
 end
 
 def division(num1, num2)
+  puts "Enter first number"
+  num1 == gets.chomp()
+  puts "Enter second number"
+  num2 == gets.chomp()
+  
+  if division == "/"
+    puts (num1 / num2)
 
 end
 
 def multiplication(num1, num2)
+  puts "Enter first number"
+  num1 == gets.chomp()
+  puts "Enter second number"
+  num2 == gets.chomp()
+  
+  if multiplication == "*"
+    puts (num1 * num2)
+    
 
 end
 
